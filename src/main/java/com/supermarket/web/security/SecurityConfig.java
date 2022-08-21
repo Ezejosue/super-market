060436f72b6b27009717b3ad3b9c65e38065ea29
Josué Ávalos <aezequiel56@gmail.com>
2022-08-20 19:45:17 -0600

    Update SecurityConfig.java

--- src/main/java/com/supermarket/web/security/SecurityConfig.java
+++ src/main/java/com/supermarket/web/security/SecurityConfig.java
@@ -37,8 +37,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   public void configure(WebSecurity web) throws Exception {
-    web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "swagger-resources/**",
-        "/configuration/security", "/swagger-ui/index.html", "/webjars/**");
+    web.ignoring().antMatchers("/v2/api-docs/**", "/configuration/ui", "swagger-resources/**",
+        "/configuration/security", "/swagger.json", "/swagger-ui/index.html", "/webjars/**");
   }
 
   @Override
