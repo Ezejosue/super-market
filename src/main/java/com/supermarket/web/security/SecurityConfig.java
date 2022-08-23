7ca5aeb7ae4f0ff4a6989861f2af7d5533d39d53
Josué Ávalos <aezequiel56@gmail.com>
2022-08-22 19:59:07 -0600

    Configuración de la seguridad para permitir acceso a la ui de swagger

--- src/main/java/com/supermarket/web/security/SecurityConfig.java
+++ src/main/java/com/supermarket/web/security/SecurityConfig.java
@@ -22,6 +22,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Autowired
   private JwtFilterRequest jwtFilterRequest;
 
+  private static final String[] AUTH_WHITELIST = {
+      // -- Swagger UI v2
+      "/v2/api-docs",
+      "/swagger-resources",
+      "/swagger-resources/**",
+      "/configuration/ui",
+      "/configuration/security",
+      "/swagger-ui.html",
+      "/webjars/**",
+      // -- Swagger UI v3 (OpenAPI)
+      "/v3/api-docs/**",
+      "/swagger-ui/**"
+      // other public endpoints of your API may be appended to this array
+  };
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
     auth.userDetailsService(userDetailsService);
@@ -29,7 +43,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.csrf().disable().authorizeRequests().antMatchers("/**/authenticate").permitAll()
+    http.csrf().disable().authorizeRequests().antMatchers(AUTH_WHITELIST).permitAll().antMatchers("/**/authenticate").permitAll()
         .anyRequest().authenticated().and().sessionManagement().sessionCreationPolicy(
             SessionCreationPolicy.STATELESS);
     http.addFilterBefore(jwtFilterRequest, UsernamePasswordAuthenticationFilter.class);
@@ -37,8 +51,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   public void configure(WebSecurity web) throws Exception {
-    web.ignoring().antMatchers("/v2/api-docs/**", "/configuration/ui", "swagger-resources/**",
-        "/configuration/security", "/swagger.json", "/swagger-ui/index.html", "/webjars/**");
+    web.ignoring().antMatchers("/v2/api-docs", "/swagger-resources/**","/configuration/ui",
+        "/configuration/security","/swagger-ui.html","/swagger.json", "/swagger-ui/index.html", "/webjars/**");
   }
 
   @Override
